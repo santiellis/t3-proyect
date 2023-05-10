d98924d03e5971ef4e06b80d703c0041e6890b39
santiellis <Santiagoellis@hotmail.com.ar>
2023-05-10 16:52:24 -0300

    New Files for new routes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -93,11 +93,11 @@ const PostView = (props: PostWithUser) => {
         />
         <div className="flex flex-col">
           <div className="flex gap-1 text-slate-300">
-            <span>{`@${author.username}`}</span>
-            <span className="font-thin">
+          <Link href={`/@${author.username}`}><span>{`@${author.username}`}</span></Link>
+          <Link href={`/post/${post.id}`}> <span className="font-thin">
               {" "}
               {`· ${dayjs(post.createdAt).fromNow()}`}{" "}
-            </span>
+            </span></Link>
           </div>
           <span className="text-2xl">{post.content}</span>
         </div>
