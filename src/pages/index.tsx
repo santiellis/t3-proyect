c8985e7f9647d1578bab65dba5758ad05419a1b0
santiellis <Santiagoellis@hotmail.com.ar>
2023-05-10 19:49:51 -0300

    Render Post on profile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,14 @@
 import { type NextPage } from "next";
 import { SignIn, SignInButton, useUser } from "@clerk/nextjs";
-import Head from "next/head";
-import Link from "next/link";
-
 import { api } from "~/utils/api";
-import type { RouterOutputs } from "~/utils/api";
 import toast from "react-hot-toast";
 
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
 import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { PageLayout } from "~/components/layout";
+import { PostView } from "~/components/postview";
 
-dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
@@ -78,34 +72,6 @@ const CreatePostWizard = () => {
   );
 };
 
-type PostWithUser = RouterOutputs["post"]["getAll"][number];
-
-const PostView = (props: PostWithUser) => {
-  const { post, author } = props;
-  return (
-    <div className="flex flex-col">
-      <div className="flex gap-3 border-b border-slate-400 p-4" key={post.id}>
-        <Image
-          src={author.profileImageUrl}
-          alt={`@${author.username}' profile picture`}
-          className="h-14 w-14 rounded-full"
-          width={56}
-          height={56}
-        />
-        <div className="flex flex-col">
-          <div className="flex gap-1 text-slate-300">
-          <Link href={`/@${author.username}`}><span>{`@${author.username}`}</span></Link>
-          <Link href={`/post/${post.id}`}> <span className="font-thin">
-              {" "}
-              {`· ${dayjs(post.createdAt).fromNow()}`}{" "}
-            </span></Link>
-          </div>
-          <span className="text-2xl">{post.content}</span>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 const Feed = () => {
   const { data, isLoading: postLoading } = api.post.getAll.useQuery();
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -2,15 +2,43 @@ import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 
-import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
-
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 import { Ratelimit } from "@upstash/ratelimit"; // for deno: see above
 import { Redis } from "@upstash/redis";
 import { filterUserForClient } from "~/server/helpers/filterUserForClient";
+import type { Post } from "@prisma/client";
 
+const addUsersDataToPosts = async (posts: Post[]) => {
+  const users = (
+    await clerkClient.users.getUserList({
+      userId: posts.map((post) => post.authorId),
+      limit: 100,
+    })
+  ).map(filterUserForClient);
 
+  return posts.map((post) => {
+    const author = users.find((user) => user.id === post.authorId);
 
+    if (!author || !author.username)
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Author for post not Found",
+      });
+
+    return {
+      post,
+      author: {
+        ...author,
+        username: author.username,
+      },
+    };});
+
+}
 
 // Create a new ratelimiter, that allows 3 requests per 1 minute
 const ratelimit = new Ratelimit({
@@ -21,60 +49,59 @@ const ratelimit = new Ratelimit({
    * Optional prefix for the keys used in redis. This is useful if you want to share a redis
    * instance with other applications and want to avoid key collisions. The default prefix is
    * "@upstash/ratelimit"
-   */ 
+   */
   prefix: "@upstash/ratelimit",
 });
 
 export const postRouter = createTRPCRouter({
- getAll: publicProcedure.query(async ({ ctx }) => {
+  getAll: publicProcedure.query(async ({ ctx }) => {
     const posts = await ctx.prisma.post.findMany({
       take: 100,
-      orderBy: [
-        {createdAt: "desc"}
-      ]
+      orderBy: [{ createdAt: "desc" }],
     });
 
-    const users = (
-      await clerkClient.users.getUserList({
-      userId : posts.map((post) => post.authorId),
-      limit: 100,
-    })
-    ).map(filterUserForClient)
-
-    return posts.map((post) =>  {
-      
-      const author = users.find((user) => user.id === post.authorId)
-
-      if(!author || !author.username) throw new TRPCError({code : "INTERNAL_SERVER_ERROR", message: "Author for post not Found"})
-      
-      return {
-      post,
-      author: {
-        ...author,
-        username: author.username,
-      },
-    }})
-
-  }),
-
-  create : privateProcedure.input(z.object({
-    content: z.string().emoji("Only emojis are allowed").min(1).max(280),
-  })).mutation(async({ctx, input}) =>{
-
-    const authorId = ctx.userId;
-
-    const { success } = await ratelimit.limit(authorId);
+    return addUsersDataToPosts(posts)
+   
     
-    if (!success) {
-      throw new TRPCError({code: "TOO_MANY_REQUESTS"})
-    }
-
-    const post = await ctx.prisma.post.create({
-      data: {
-        authorId,
-        content: input.content,
-      }
-    });
-    return post;
   }),
+
+  getPostsByUserId: publicProcedure
+    .input(
+      z.object({
+        userId: z.string(),
+      })
+    )
+    .query(({ ctx, input }) =>
+      ctx.prisma.post.findMany({
+        where: {
+          authorId: input.userId,
+        },
+        take: 100,
+        orderBy: [{ createdAt: "desc" }],
+      }).then(addUsersDataToPosts)
+    ),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji("Only emojis are allowed").min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      const { success } = await ratelimit.limit(authorId);
+
+      if (!success) {
+        throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
+      }
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          content: input.content,
+        },
+      });
+      return post;
+    }),
 });
